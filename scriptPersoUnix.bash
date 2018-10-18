fabc50d831269a995bfbad0521f792785d66fd6f
sali6000 <34247058+sali6000@users.noreply.github.com>
2018-10-19 01:49:00 +0200

    Update scriptPersoUnix.bash

--- scriptPersoUnix.bash
+++ scriptPersoUnix.bash
@@ -22,5 +22,11 @@ sudo apt install adobe-flashplugin browser-plugin-freshplayer-pepperflash
 # Importer dans Firefox les favoris sauvegardés
 mv /home/ubuntu/Desktop/Ubuntu-Linux-master/bookmarks-2018-10-18_413_2yOFe0jj0pXUcc010Gp4TA\=\=.jsonlz4 /home/ubuntu/.mozilla/firefox/*.default/bookmarkbackups/
 
-# Installer AdBlock
+# Install and show Keyboard Virtual
+sudo apt install onboard
+
+# Ouvrir le clavier virtuel
+onboard
+
+# Ouvrir Firefox et installer AdBlock
 sudo firefox https://addons.mozilla.org/firefox/downloads/file/1061111/adblock_plus-3.3.1-an+fx.xpi www.hds.to www.youtube.com www.google.com www.suptnik.com

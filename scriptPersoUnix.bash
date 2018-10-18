1b38d0d2d1d9e20486edf9a459e41a5d4a3473ad
sali6000 <34247058+sali6000@users.noreply.github.com>
2018-10-19 01:53:06 +0200

    Update scriptPersoUnix.bash

--- scriptPersoUnix.bash
+++ scriptPersoUnix.bash
@@ -3,6 +3,12 @@
 # Couper Firefox
 pkill -f firefox
 
+# Install and show Keyboard Virtual
+sudo apt install onboard
+
+# Ouvrir le clavier virtuel
+onboard
+
 # Mettre le clavier en francais (belge).
 setxkbmap be
 
@@ -10,7 +16,7 @@ setxkbmap be
 sudo add-apt-repository universe
 sudo add-apt-repository multiverse
 
-# Ajouter le server où se trouve flashPlayer
+# Ajouter le server où se trouve flashPlayer dans la liste des servers de téléchargement de packages
 sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
 
 # Mettre à jour
@@ -21,12 +27,6 @@ sudo apt install adobe-flashplugin browser-plugin-freshplayer-pepperflash
 
 # Importer dans Firefox les favoris sauvegardés
 mv /home/ubuntu/Desktop/Ubuntu-Linux-master/bookmarks-2018-10-18_413_2yOFe0jj0pXUcc010Gp4TA\=\=.jsonlz4 /home/ubuntu/.mozilla/firefox/*.default/bookmarkbackups/
-
-# Install and show Keyboard Virtual
-sudo apt install onboard
-
-# Ouvrir le clavier virtuel
-onboard
 
 # Ouvrir Firefox et installer AdBlock
 sudo firefox https://addons.mozilla.org/firefox/downloads/file/1061111/adblock_plus-3.3.1-an+fx.xpi www.hds.to www.youtube.com www.google.com www.suptnik.com
